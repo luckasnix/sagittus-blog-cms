6a1741685f3e5544445ccb471cc73d9687c5dd6a
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-11-18 10:27:25 -0300

    Add validation rules to category type

--- schemas/types/category.js
+++ schemas/types/category.js
@@ -6,12 +6,18 @@ export default {
     {
       name: 'slug',
       title: 'Slug',
-      type: 'slug'
+      type: 'slug',
+      options: {
+        source: 'title',
+        slugify: input => input.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/\s+/g, '-')
+      },
+      validation: Rule => Rule.required().error('O slug é obrigatório')
     },
     {
       name: 'title',
       title: 'Título',
-      type: 'string'
+      type: 'string',
+      validation: Rule => Rule.required().error('O título é obrigatório')
     }
   ]
 }
