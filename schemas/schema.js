0cd4106ae79abb318885e64737c2539ed0a56a22
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-11-21 20:08:51 -0300

    Create the metadata type with validation rules

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,10 +1,16 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import metadata from './types/metadata'
 import author from './types/author'
 import category from './types/category'
 import post from './types/post'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([ author, category, post ])
+  types: schemaTypes.concat([
+    metadata,
+    author,
+    category,
+    post
+  ])
 })
---
+++ schemas/types/metadata.js
@@ -0,0 +1,68 @@
+export default {
+  name: 'metadata',
+  title: 'Metadados',
+  type: 'document',
+  fields: [
+    {
+      name: 'organization',
+      title: 'Organização',
+      type: 'string',
+      validation: Rule => Rule.required().error('A organização é obrigatória')
+    },
+    {
+      name: 'title',
+      title: 'Título',
+      type: 'string',
+      validation: Rule => [
+        Rule.required().error('O título é obrigatório'),
+        Rule.max(60).warning('O título ideal deve conter no máximo 60 caracteres')
+      ]
+    },
+    {
+      name: 'description',
+      title: 'Descrição',
+      type: 'text',
+      rows: 2,
+      validation: Rule => [
+        Rule.required().error('A descrição é obrigatória'),
+        Rule.min(50).warning('A descrição ideal deve conter no mínimo 50 caracteres'),
+        Rule.max(160).warning('A descrição ideal deve conter no máximo 160 caracteres')
+      ]
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'string',
+      validation: Rule => [
+        Rule.required().error('A URL é obrigatória'),
+        Rule.uri({
+          scheme: ['http', 'https']
+        }).error('A URL deve iniciar com "http" ou "https"')
+      ]
+    },
+    {
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      validation: Rule => Rule.required().error('A logo é obrigatória')
+    },
+    {
+      name: 'locale',
+      title: 'Localidade',
+      type: 'string',
+      validation: Rule => Rule.required().error('A localidade é obrigatória')
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: Rule => Rule.required().error('O email é obrigatório')
+    },
+    {
+      name: 'phone',
+      title: 'Telefone',
+      type: 'string',
+      validation: Rule => Rule.required().error('O telefone é obrigatório')
+    }
+  ]
+}
